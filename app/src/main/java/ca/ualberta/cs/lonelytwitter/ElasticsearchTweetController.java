66a96c20a2a4d617bd110e4988a6a5bdcff425fd
HarleyVanselow <code@harleyv.net>
2016-10-20 17:42:28 -0600

    Basics

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -1,10 +1,41 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Index;
+
 /**
  * Created by esports on 2/17/16.
  */
 public class ElasticsearchTweetController {
-    //TODO: A function that gets tweets
+    private static JestDroidClient client;
 
+    //TODO: A function that gets tweets
+    public static class AddTweetsTask extends AsyncTask<NormalTweet,Void,Void>{
+        protected Void doInBackground(NormalTweet... tweets) {
+            Index index = new Index.Builder(tweets[0]).index("testing").type("tweet").build();
+            try{
+
+            }catch(Exception e){
+                Log.i("Error","The application failed to build and send the tweet");
+            }
+            return null;
+        }
+    }
     //TODO: A function that adds a tweet
+    public static void verifySettings(){
+        if(client == null){
+            DroidClientConfig.Builder builder =new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+            DroidClientConfig config = builder.build();
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient)factory.getObject();
+        }
+    }
 }
